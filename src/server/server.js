caa207107a8bbf3a78a25236c081e0eb28e21bb9
kostja93 <k.gontscharow@gmail.com>
2020-06-08 07:46:55 +0200

    register oracles on start up really

--- src/server/server.js
+++ src/server/server.js
@@ -8,15 +8,21 @@ let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+const oracles = []
 
-async function registerOracles() {
-  for(i = 0; i < 10; i++) {
-    let oracle = web3.eth.accounts[i + 1]
-    let fee = web3.utils.toWei('1', 'ether')
-    await flightSuretyApp.registerOracle({ from: oracle, value: fee })
-    let result = await flightSuretyApp.getMyIndexes.call({ from: oracle })
-    console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`)
-  }
+function registerOracles(accounts) {
+  let fee = web3.utils.toWei('1', 'ether')
+  accounts.forEach( (oracle) => {
+    console.log('Registrating: ', oracle)
+    flightSuretyApp.methods.registerOracle().send({ from: oracle, value: fee, gas: 3000000 }, (err) => {
+      if (err) return console.log(err)
+      flightSuretyApp.methods.getMyIndexes().call({ from: oracle }, (err, result) => {
+        if (err) return console.log(err)
+        oracles.push([ result[0], result[1], result[3] ])
+        console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`)
+      })
+    })
+  })
 }
 
 flightSuretyApp.events.OracleRequest({ fromBlock: 0 }, function (error, event) {
@@ -25,11 +31,14 @@ flightSuretyApp.events.OracleRequest({ fromBlock: 0 }, function (error, event) {
 });
 
 const app = express();
+web3.eth.personal.getAccounts().then(registerOracles);
+
 app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
+  res.send({
+    message: 'An API for use with your Dapp!'
+  })
 })
+
 
 export default app;
 
