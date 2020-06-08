68fbc33ace606f117e787ad997178aef988c88d5
kostja93 <k.gontscharow@gmail.com>
2020-06-08 06:39:47 +0200

    register oracles on start up

--- src/server/server.js
+++ src/server/server.js
@@ -9,12 +9,19 @@ let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('htt
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
+async function registerOracles() {
+  for(i = 0; i < 10; i++) {
+    let oracle = web3.eth.accounts[i + 1]
+    let fee = web3.utils.toWei('1', 'ether')
+    await flightSuretyApp.registerOracle({ from: oracle, value: fee })
+    let result = await flightSuretyApp.getMyIndexes.call({ from: oracle })
+    console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`)
+  }
+}
 
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
+flightSuretyApp.events.OracleRequest({ fromBlock: 0 }, function (error, event) {
+  if (error) console.log(error)
+  console.log('########## Oracle Request Event Emitted #######################################')
 });
 
 const app = express();
