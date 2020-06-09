a15c195ee25a22389969908d56a7cf7001479ead
kostja93 <k.gontscharow@gmail.com>
2020-06-09 06:32:03 +0200

    update flight status with oracle

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,7 @@ let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('htt
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 const oracles = []
+const flightStatuses = [10, 20, 30, 40, 50]
 
 function registerOracles(accounts) {
   let fee = web3.utils.toWei('1', 'ether')
@@ -18,16 +19,26 @@ function registerOracles(accounts) {
       if (err) return console.log(err)
       flightSuretyApp.methods.getMyIndexes().call({ from: oracle }, (err, result) => {
         if (err) return console.log(err)
-        oracles.push([ result[0], result[1], result[3] ])
+        oracles.push([ oracle, result[0], result[1], result[2] ])
         console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`)
       })
     })
   })
 }
 
+function fetchFlightStatus(event) {
+  const { airline, flight, timestamp, index } = event.returnValues
+  const flightStatus = flightStatuses[ Math.floor( Math.random() * 5 ) ]
+  const oracle = oracles.find(o => o[1] == index || o[2] == index || o[3] == index)[0]
+  flightSuretyApp.methods.submitOracleResponse(index, airline, flight, timestamp, flightStatus).send({ from: oracle });
+  console.log(oracle, flightStatus)
+  console.log('########## Updated flight status ##############################################')
+}
+
 flightSuretyApp.events.OracleRequest({ fromBlock: 0 }, function (error, event) {
   if (error) console.log(error)
   console.log('########## Oracle Request Event Emitted #######################################')
+  fetchFlightStatus(event)
 });
 
 const app = express();
